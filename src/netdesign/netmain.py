4869430540430d7456151b5554b60f78e6db28ce
木雨亏 <chuxing0119@gmail.com>
2017-09-19 14:04:31 +0800

    new second

--- src/netdesign/netmain.py
+++ src/netdesign/netmain.py
@@ -4,6 +4,7 @@ import numpy as np
 import codecs
 from collections import defaultdict
 from heapq import *
+import json
 
 def loadDataset(infile):
     df = pd.read_csv(infile, sep = '\t', header = None, dtype = str, na_filter = False)
@@ -95,16 +96,54 @@ def prime(vertexs,deges):
 
     return mst
 
-def getedgeod(startnode,root,alledges,nodedict,data_od_matrix):
+def getcityid(nownode,nodedict):
+    onenodecityid = []
+    for k,v in nodedict[nownode][2].items():
+        citynode = k-4
+        onenodecityid.append(citynode)
+    return onenodecityid
 
+def getcityod(sid,eid,data_od_matrix):
+    sumod = 0
+    for i in sid:
+        for j in eid:
+            sumod += data_od_matrix[i][j]
+    return  sumod
+
+def getedgeod(startnode,root,alledges,nodedict,data_od_matrix):
+    startcityid = getcityid(startnode,nodedict)
+    flag = [0 for i in range(120)]
+    flag[startnode] = 1
+    nodeuse = []
+    nodeuse.append(root)
+    sumod = 0
+    while len(nodeuse) != 0:
+        nownode = nodeuse[0]
+        flag[nownode] = 1
+        del nodeuse[0]
+        nowcityid = getcityid(nownode,nodedict)
+        cityod = getcityod(startcityid,nowcityid,data_od_matrix)
+        sumod += cityod
+        for v in alledges[nownode]:
+            newnode = v[0]
+            if flag[newnode] == 1:
+                continue
+            nodeuse.append(newnode)
+    return  sumod
 
 def output(data_x,data_od,data_od_matrix,nodedict,firstnodedict,secondenodedict,mst):
     outlist = []
     alledges = mst
+    f = open("../../data/linksresult/links.json", 'w')
     for k,v in secondenodedict.items():
         alledges[k] = v
         alledges[v[0][0]].append((k,v[0][1]))
+    tmpmax = 0
+    travelcost = 0.0
+    devicecost = 0.0
+    tunnelcost = 0.0
     for k,v in nodedict.items():
+        print(k,v)
         outdict = {}
         outdict["node_id"] = k
         if v[1] > 3000:
@@ -113,6 +152,8 @@ def output(data_x,data_od,data_od_matrix,nodedict,firstnodedict,secondenodedict,
             outdict["node_status"] = 2
         outdict["node_axis"] = v[0]
         outdict["node_capacity"] = v[1]
+        outdict["node_cost"] = 150000000 / (36500*1.0) if outdict["node_status"] == 1 else (100000000 / (36500*1.0))
+        devicecost += outdict["node_cost"]
         links = alledges[k]
         linkslist = []
         for link in links:
@@ -120,11 +161,24 @@ def output(data_x,data_od,data_od_matrix,nodedict,firstnodedict,secondenodedict,
             endnode = link[0]
             distance = link[1]
             edgeod = getedgeod(k,endnode,alledges,nodedict,data_od_matrix)
+            tmpmax = max(tmpmax,edgeod)
             tmpdict["endnode"] = endnode
             tmpdict["distance"] = distance
             tmpdict["edgeod"] = edgeod
+            tmpdict["cost_travel"] = distance / 1000.0 * edgeod
+            tmpdict["cost_tunnel"] = (350000000 if edgeod > 3600 else 300000000) * (distance / 1000.0) / (36500*1.0)
+            tunnelcost += tmpdict["cost_tunnel"]
+            travelcost += tmpdict["cost_travel"]
             linkslist.append(tmpdict)
         outdict["links"] = linkslist
+        outlist.append(outdict)
+    outlist.append({"maxtunnelod": tmpmax})
+    outlist.append({"devicecost": devicecost})
+    outlist.append({"travelcost": travelcost})
+    outlist.append({"tunnelcost": tunnelcost / 2})
+    outlist.append({"allcost":(devicecost + travelcost + tunnelcost / 2)})
+    print(tmpmax,(devicecost + travelcost + tunnelcost / 2 ))
+    json.dump(outlist,f,indent = 2)
 
 
 if __name__ == "__main__":
@@ -145,6 +199,6 @@ if __name__ == "__main__":
         for k2,v2 in v.items():
             edges.append((k,k2,v2))
     mst = prime(vertexs,edges)
-    print(mst)
+    # print(mst)
     # print(firstnodedict,secondenodedict)
     output(data_x,data_od,data_od_matrix,nodedict,firstnodedict,secondenodedict,mst)
